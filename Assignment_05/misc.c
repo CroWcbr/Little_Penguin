de4de87f59726ce857cadecb10d784d5dc3636d7
Vitaliy Shavnin <crowf4i@gmail.com>
2023-10-02 00:25:55 +0200

    update

--- Assignment_05/misc.c
+++ Assignment_05/misc.c
@@ -17,58 +17,58 @@ static char data[LOGIN_LEN];
 
 static int my_open(struct inode *inode, struct file *file)
 {
-		printk("misc device my_open : open called\n");
-		printk("misc device my_open : device number %d %d\n", imajor(inode), iminor(inode));
-		if (file->f_mode & FMODE_READ)
-				printk("misc device my_open : open called with read permission\n");
-		if (file->f_mode & FMODE_WRITE)
-				printk("misc device my_open : open called with write permission\n");\
-		return 0;
+        printk("misc device my_open : open called\n");
+        printk("misc device my_open : device number %d %d\n", imajor(inode), iminor(inode));
+        if (file->f_mode & FMODE_READ)
+                printk("misc device my_open : open called with read permission\n");
+        if (file->f_mode & FMODE_WRITE)
+                printk("misc device my_open : open called with write permission\n");\
+        return 0;
 }
 
 static int my_close(struct inode *inode, struct file *file)
 {
-		printk("misc device my_close : close called\n");
-		return 0;
+        printk("misc device my_close : close called\n");
+        return 0;
 }
 
 static ssize_t my_write(struct file *file, const char __user *user_buffer, size_t user_len, loff_t *ppos)
 {
-		printk("misc device my_write : write called\n");
-		if (LOGIN_LEN != user_len || strncmp(LOGIN, user_buffer, user_len))
-		{
-			printk("misc device my_write : invalid value\n");
-			return -1;
-		}
-		int status = copy_from_user(data, user_buffer, user_len);
-		if (status)
-		{
-				printk("misc device my_write : error during copy_from_user\n");
-				return -status;
-		}
-		return user_len;
+        printk("misc device my_write : write called\n");
+        if (LOGIN_LEN != user_len || strncmp(LOGIN, user_buffer, user_len))
+        {
+            printk("misc device my_write : invalid value\n");
+            return -1;
+        }
+        int status = copy_from_user(data, user_buffer, user_len);
+        if (status)
+        {
+                printk("misc device my_write : error during copy_from_user\n");
+                return -status;
+        }
+        return user_len;
 }
 
 static ssize_t my_read(struct file *file, char __user *user_buffer, size_t user_len, loff_t *ppos)
 {
-		printk("misc device my_read : read called\n");
-		int len = user_len < LOGIN_LEN - *ppos ? user_len : LOGIN_LEN - *ppos;
-		int status = copy_to_user(user_buffer, data + *ppos, len);
-		if (status)
-		{
-				printk("misc device my_write : error during copy_to_user\n");
-				return -status;
-		}
-		*ppos += len;
-		return len;
+        printk("misc device my_read : read called\n");
+        int len = user_len < LOGIN_LEN - *ppos ? user_len : LOGIN_LEN - *ppos;
+        int status = copy_to_user(user_buffer, data + *ppos, len);
+        if (status)
+        {
+                printk("misc device my_write : error during copy_to_user\n");
+                return -status;
+        }
+        *ppos += len;
+        return len;
 }
 
 static const struct file_operations fops = {
-		.owner = THIS_MODULE,
-		.read = my_read,
-		.write = my_write,
-		.open = my_open,
-		.release = my_close,
+        .owner = THIS_MODULE,
+        .read = my_read,
+        .write = my_write,
+        .open = my_open,
+        .release = my_close,
 };
 
 static struct miscdevice my_device = {
@@ -84,11 +84,11 @@ static int __init my_init(void)
 
         printk(KERN_INFO "misc device my_init : register misc device!\n");
         status = misc_register(&my_device);
-		if (status)
-		{
-				printk("misc device my_init : error during register misc device\n");
-				return -status;
-		}
+        if (status)
+        {
+                printk("misc device my_init : error during register misc device\n");
+                return -status;
+        }
         return 0;
 }
 
@@ -96,7 +96,7 @@ static int __init my_init(void)
 static void __exit my_exit(void)
 {
         printk(KERN_INFO "misc device my_exit : deregister misc device\n");
-		misc_deregister(&my_device);
+        misc_deregister(&my_device);
 }
 
 module_init(my_init);
---
+++ Assignment_06/README.md
@@ -0,0 +1,11 @@
+## Assignment 00
+
+### Task
+- Download the linux-next kernel for today. Or tomorrow, just use the latest one. It changes every day so there is no specific one you need to pick. Build it. Boot it.
+
+### Instraction
+- download latest kernel from https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+- task like Assignment_00
+
+### Help
+- task like Assignment_00
